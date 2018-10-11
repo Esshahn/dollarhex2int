7634be783bcbd6a708f16c07299ffe5b14df550e
Ingo Hinterding <ingo@hinterding.de>
2018-10-11 18:07:20 +0200

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+hex2int Preview
+================
+
+## Summary
+This plugin show int value of hex in statusbar (just put cursor on hex value)
+
+## Screenshot
+![ScreenShot](https://raw.github.com/unknownuser88/hex2int/master/screenshot.png)
+
+## Install
+
+#### Git Clone
+Clone this repository in to the Sublime Text "Packages" directory, which is located where ever the
+"Preferences" -> "Browse Packages" option in sublime takes you.
+
+
+
+---
---
+++ dollarhext2int.py
@@ -0,0 +1,43 @@
+# https://github.com/unknownuser88/hex2int
+# based on hex2int by unknownuser88
+
+import sublime, sublime_plugin, re
+
+class HtointCommand(sublime_plugin.EventListener):
+	def on_new(self, view):
+		self.run(view)
+
+	def on_selection_modified(self, view):
+		self.run(view)
+
+	def on_activated(self, view):
+		self.run(view)
+
+	def run(self, view):
+		statusline = []
+		for region in view.sel():
+			
+			if region.begin() == region.end():
+				word = view.word(region)
+			else:
+				word = region
+
+			if not word.empty():
+
+				keyword = view.substr(word)
+				isHex = re.findall(r'\$|[0-9a-fA-F]+', keyword)
+
+				if isHex:
+					isHex[0] = isHex[0].replace("$","0x")
+					statusline.append('{} -> {}'.format(isHex[0], self.str_to_int(isHex[0])))
+
+		view.erase_status('Hexcode')
+		view.set_status('Hexcode', '{}'.format(", ".join(statusline)))
+
+	def str_to_int(self, s):
+		i = int(s, 16)
+		if i >= 2**23:
+			i -= 2**24
+		return i
+
+
